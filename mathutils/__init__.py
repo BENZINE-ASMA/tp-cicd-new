14219ea503ca8864175cb58aa07f5c21c2e2f68c
BENZINE-ASMA <asma.benzine01@gmail.com>
2025-11-29 13:50:59 +0100

    Initial commit for new repo

---
+++ mathutils/__init__.py
@@ -0,0 +1,22 @@
+"""Simple mathematical utilities for CI/CD learning."""
+
+__version__ = "0.1.0"
+
+from .calculator import (
+    add,
+    divide,
+    factorial,
+    multiply,
+    power,
+    square_root,
+)
+
+__all__ = [
+    "add",
+    "subtract",
+    "multiply",
+    "divide",
+    "power",
+    "square_root",
+    "factorial",
+]
